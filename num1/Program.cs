66e9db9a3ea76bf8ec86b7e4a8d3743ceec92916
SeregaLi <kaprall13rus@gmail.com>
2022-09-20 21:23:09 +0300

    first commit

---
+++ num1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2)
+{
+    Console.WriteLine($"Максимальное число {number1} минимальное число {number2}");
+}
+else if (number1 > number2)
+{
+    Console.WriteLine($"Максимальное число {number1} минимальное число {number2}");
+}
+
+else 
+{
+    Console.WriteLine($"Максимальное число {number2} минимальное число {number1}");
+}
