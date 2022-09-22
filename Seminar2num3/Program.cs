631badf782a80968bc6a42ef72b9b78da664e028
SeregaLi <kaprall13rus@gmail.com>
2022-09-22 20:30:31 +0300

    + дз 2 семинара финал

---
+++ Seminar2num3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+string[] weekDays = new string[] { "нет", "нет", "нет", "нет", "нет", "да", "да" };
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine()); 
+Console.Write(weekDays[number - 1]);
+
