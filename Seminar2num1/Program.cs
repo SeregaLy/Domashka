6448db2cb77d8313cbbe4c6c35b70a01b450a5da
SeregaLi <kaprall13rus@gmail.com>
2022-09-22 20:16:52 +0300

    + дз 2 семинара

---
+++ Seminar2num1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число N");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int twoNum = number % 10 % 10;
+
+Console.Write(twoNum);
+
---
+++ Seminar2num2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int lenght = number.Length;
+if (lenght > 3) 
+{
+    Console.Write(Convert.ToInt32(number) / 100 % 10);  
+}
+else 
+{ 
+    Console.Write("третьей цифры нет");
+}
