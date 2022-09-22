2287aae77ac5a83a7031ef6eed43c2bb526b866f
SeregaLi <kaprall13rus@gmail.com>
2022-09-22 21:36:33 +0300

    + задача с семинара 18

---
+++ Tasck18/Program.cs
@@ -0,0 +1,34 @@
+﻿string getQuarterFromCoordinate(int quarter)
+{
+    string coordinate = "";
+    if (quarter == 1)
+    {
+        coordinate = "x > 0 y > 0";
+    }
+    else if (quarter == 2)
+    {
+        coordinate = "x < 0 y > 0";
+    }
+    else if (quarter == 3)
+    {
+        coordinate = "x < 0 y < 0";
+    }
+    else if (quarter == 4)
+    {
+        coordinate = "x > 0 y < 0";
+    }
+
+    return coordinate;
+}
+string dip;
+Console.WriteLine("Введите номер четверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+if (quarter > 0 && quarter < 5)
+{
+    dip = getQuarterFromCoordinate(quarter);
+    Console.WriteLine($"Диапазон значений {dip}");
+}
+else
+{
+    Console.WriteLine($"Неверное значение");
+}
