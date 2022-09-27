e81b57443c2add37463e2b13c4f7738ac40f5be3
SeregaLi <kaprall13rus@gmail.com>
2022-09-27 08:28:43 +0300

    +решение 1 задачи дз симинара №3

---
+++ DZ_seminar_3-1/Program.cs
@@ -0,0 +1,24 @@
+﻿int getNumFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation} ");
+    }
+    return result;
+}
+
+string checkNumber(int num)
+{
+if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10)
+    {
+        return "Да";
+    }
+else
+    {
+        return "Нет";
+    }
+}
+int num = getNumFromUser("Введите пятизначное число");
+Console.Write(checkNumber(num));
