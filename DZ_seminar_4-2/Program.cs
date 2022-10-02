9d4e221f6ed45bb1c6c488d6e64d7b85f5a275aa
SeregaLi <kaprall13rus@gmail.com>
2022-10-02 22:04:06 +0300

    +решение 2 задачи дз симинара №4

---
+++ DZ_seminar_4-2/Program.cs
@@ -0,0 +1,25 @@
+﻿int getNumFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation} ");
+    }
+    return result;
+}
+
+int sumNum(int num)
+{
+    int result = 0;
+    while(num > 0)
+    {
+        result += num % 10;
+        num /= 10;
+    }
+    return result;
+}
+
+int num = getNumFromUser("Введите целое число: ");
+int sumNumUser = sumNum(num);
+Console.Write($"сумма цифр введеного числа = {sumNumUser}");
