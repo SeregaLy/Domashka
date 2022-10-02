805e71e8c16a147cd334c60f49fdc524a4c5d359
SeregaLi <kaprall13rus@gmail.com>
2022-10-02 22:30:26 +0300

    +решение 3 задачи дз симинара №4

---
+++ DZ_seminar_4-3/Program.cs
@@ -0,0 +1,37 @@
+﻿const int START = 1;
+const int END = 99;
+int getNumFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число, больше единицы. {userInformation} ");
+    }
+    return result;
+}
+int[] rundomArray(int length, int start, int end)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(start, end + 1);
+    }
+    return resultArray;
+}
+void printNum(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+int lengthArray = getNumFromUser("Введите длинну массива: ");
+int[] newRundomArray = rundomArray(lengthArray, START, END);
+printNum(newRundomArray);
