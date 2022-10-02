1dc610a06c61cbdae8fc8975a8a85bbe4d935033
SeregaLi <kaprall13rus@gmail.com>
2022-10-02 21:24:10 +0300

    +решение 1 задачи дз симинара №4

---
+++ DZ_seminar_4-1/Program.cs
@@ -0,0 +1,21 @@
+﻿int getNumFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation} ");
+    }
+    return result;
+}
+
+double exponentNum(int Num1, int Num2)
+{
+    double result = Math. Pow(Num1, Num2);
+    return result;
+}
+
+int num1 = getNumFromUser("Введите число которое будет возводиться в степень: ");
+int num2 = getNumFromUser("Введите число которое будет степенью числа: ");
+double ExponentNum = exponentNum(num1, num2);
+Console.Write($"{ExponentNum}");
