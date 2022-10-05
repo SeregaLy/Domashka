e39c4c79836f81b6ef0972960d4533e61db390aa
SeregaLi <kaprall13rus@gmail.com>
2022-10-05 22:11:13 +0300

    +решение 2 задачи дз симинара №5

---
+++ DZ_seminar_5-2/Program.cs
@@ -0,0 +1,45 @@
+﻿const int START = -99;
+const int END = 99;
+
+int[] getRandomArray(int length, int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint + 1);
+    }
+    return resultArray;
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length -1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int sumElements(int[] currentArray)
+{
+    int result = 0;
+    for (int i = 1; i < currentArray.Length; i += 2)
+    {
+        result += currentArray[i];
+    }
+    return result;
+}
+
+Console.Write("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] currentArray = getRandomArray(length, START, END);
+printArray(currentArray);
+
+int sumElement = sumElements(currentArray);
+Console.WriteLine($"Сумма элементов на нечетных позициях = {sumElement}");
