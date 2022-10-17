21545e6018a53db3e01d5c8fed19fbc3b7762deb
SeregaLi <kaprall13rus@gmail.com>
2022-10-17 19:51:44 +0300

    +решение 1(64) задачи дз симинара №9

---
+++ DZ-seminar_9-1-64/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+
+int getNumFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result) || result <= 1)
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число, больше единицы. {userInformation} ");
+    }
+    return result;
+}
+
+void PrintRange (int number)
+{
+    if (number==0)
+    {
+        return ;
+    }
+    System.Console.Write(number+", ");
+    PrintRange(number-1);
+}
+
+int numberN = getNumFromUser("Введите число N: ");
+int startUserNumber = Convert.ToInt32(numberN);
+PrintRange(startUserNumber);
