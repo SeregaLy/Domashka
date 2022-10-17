23fe67c96c0470764de3cdfebaf91fc66fa4ec6f
SeregaLi <kaprall13rus@gmail.com>
2022-10-17 20:12:32 +0300

    +решение 2(66) задачи дз симинара №9

---
+++ DZ-seminar_9-2-66/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int getNumberFromUser(string userInformation)
+{
+    int result = 0;
+    Console.Write($"{userInformation} ");
+    while (!int.TryParse(Console.ReadLine(), out result))
+    {
+        Console.Write($"Ошибка ввода! Ожидается целое число. {userInformation} ");
+    }
+    return result;
+}
+
+int SumTwoNum(int M, int N)
+{
+    if (N == M)
+        {
+        return M;
+    }
+    return N + SumTwoNum(M, --N);
+    
+}
+
+int numberM = getNumberFromUser("Задайте значение М: ");
+int numberN = getNumberFromUser("Задайте значение N: ");
+int result = SumTwoNum(numberM,numberN);
+System.Console.WriteLine(result);
